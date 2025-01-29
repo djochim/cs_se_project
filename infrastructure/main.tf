562835687729060034df4cd57f3bab4d9b442378
Damien Jochim <dev@joch.im>
2025-01-29 08:24:40 +0100

    #12:  Connect cloudflare (#13)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,24 +8,15 @@ terraform {
       source  = "poseidon/ct"
       version = "0.13.0"
     }
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "5.0.0-rc1"
+    }
   }
 }
 
 provider "ct" {
   # Configuration options
-}
-
-provider "hcloud" {
-  # Configuration options
-  token = var.hcloud_token
-}
-
-variable "hcloud_token" {
-  sensitive = true
-}
-
-variable "appname" {
-  default = "aeon"
 }
 
 data "ct_config" "flatcar-ignition" {
@@ -37,15 +28,33 @@ data "template_file" "flatcar-cl-config" {
   vars     = { appname = var.appname }
 }
 
+provider "hcloud" {
+  # Configuration options
+  token = var.hcloud_token
+}
+
 # Create a new server running debian
 resource "hcloud_server" "aeon-server" {
-  name        = "aeon-server"
+  name        = var.server_name
   image       = "212121145"
-  server_type = "cx22"
-  location    = "fsn1"
+  server_type = var.server_type
+  location    = var.server_location
   user_data   = data.ct_config.flatcar-ignition.rendered
   public_net {
     ipv4_enabled = true
     ipv6_enabled = true
   }
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_token
+}
+
+resource "cloudflare_dns_record" "main_dns" {
+  zone_id = var.cloudflare_zone_id
+  name    = var.domain
+  content  = var.ip
+  ttl = 1
+  type    = "A"
+  proxied = true
 }
