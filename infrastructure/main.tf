e8b7e138f70f98fbe3aec24a8e00e494b8d30948
Damien Jochim <dev@joch.im>
2025-02-03 21:09:23 +0100

    #14: Add secure zone settings and generate ssl for mtls (#15)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,10 +9,39 @@ terraform {
       version = "0.13.0"
     }
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
       version = "5.0.0-rc1"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.6"
+    }
   }
+}
+
+provider "tls" {
+  # Configuration options
+}
+
+# ECDSA key with P384 elliptic curve
+resource "tls_private_key" "api-cert-private-key" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P384"
+}
+
+resource "tls_cert_request" "api-cert-request" {
+  private_key_pem = tls_private_key.api-cert-private-key.private_key_pem
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_token
+}
+
+resource "cloudflare_origin_ca_certificate" "origin-cert" {
+  csr                = tls_cert_request.api-cert-request.cert_request_pem
+  hostnames          = ["*.jochim.dev", "jochim.dev"]
+  request_type       = "origin-rsa"
+  requested_validity = 365
 }
 
 provider "ct" {
@@ -25,7 +54,12 @@ data "ct_config" "flatcar-ignition" {
 
 data "template_file" "flatcar-cl-config" {
   template = file("${path.module}/flatcar-config.yaml.tmpl")
-  vars     = { appname = var.appname }
+  vars = {
+    appname           = var.appname
+    private_key       = base64encode(tls_private_key.api-cert-private-key.private_key_pem)
+    certificate       = base64encode(cloudflare_origin_ca_certificate.origin-cert.certificate)
+    nginx_conf_base64 = base64encode(file("${path.module}/nginx.conf"))
+  }
 }
 
 provider "hcloud" {
@@ -46,15 +80,41 @@ resource "hcloud_server" "aeon-server" {
   }
 }
 
-provider "cloudflare" {
-  api_token = var.cloudflare_token
-}
-
 resource "cloudflare_dns_record" "main_dns" {
   zone_id = var.cloudflare_zone_id
   name    = var.domain
-  content  = var.ip
-  ttl = 1
+  content = hcloud_server.aeon-server.ipv4_address
+  ttl     = 1
   type    = "A"
   proxied = true
+}
+
+resource "cloudflare_zone_setting" "tls1_3" {
+  zone_id    = var.cloudflare_zone_id
+  setting_id = "tls_1_3"
+  value      = "on"
+}
+
+resource "cloudflare_zone_setting" "min_tls_version" {
+  zone_id    = var.cloudflare_zone_id
+  setting_id = "min_tls_version"
+  value      = "1.2"
+}
+
+resource "cloudflare_zone_setting" "ssl" {
+  zone_id    = var.cloudflare_zone_id
+  setting_id = "ssl"
+  value      = "strict"
+}
+
+resource "cloudflare_zone_setting" "https_rewrites" {
+  zone_id    = var.cloudflare_zone_id
+  setting_id = "automatic_https_rewrites"
+  value      = "on"
+}
+
+resource "cloudflare_zone_setting" "waf" {
+  zone_id    = var.cloudflare_zone_id
+  setting_id = "waf"
+  value      = "on"
 }
