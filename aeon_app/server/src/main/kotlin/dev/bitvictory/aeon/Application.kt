5c19a0dcc0b5c96651a6b32c7a35a75cbf6ea50e
Damien Jochim <dev@joch.im>
2025-03-28 15:56:31 +0100

    16 task enable server monitoring via grafana (#67)

--- aeon_app/server/src/main/kotlin/dev/bitvictory/aeon/Application.kt
+++ aeon_app/server/src/main/kotlin/dev/bitvictory/aeon/Application.kt
@@ -2,14 +2,21 @@ package dev.bitvictory.aeon
 
 import com.typesafe.config.ConfigFactory
 import dev.bitvictory.aeon.application.applicationModule
+import dev.bitvictory.aeon.configuration.LoggingConfig
+import dev.bitvictory.aeon.infrastructure.environment.OtelEnvironment
 import dev.bitvictory.aeon.presentation.api.system
 import io.ktor.serialization.kotlinx.protobuf.protobuf
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.config.HoconApplicationConfig
 import io.ktor.server.engine.applicationEnvironment
+import io.ktor.server.plugins.calllogging.CallLogging
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.request.httpMethod
+import io.ktor.server.request.path
 import io.ktor.server.routing.routing
+import io.opentelemetry.api.trace.Span
+import io.opentelemetry.instrumentation.ktor.v3_0.KtorServerTelemetry
 import kotlinx.serialization.ExperimentalSerializationApi
 import org.koin.ktor.plugin.Koin
 import org.slf4j.LoggerFactory
@@ -23,8 +30,32 @@ fun Application.module() {
 		log = LoggerFactory.getLogger("ktor.application")
 		config = serverConfig
 	}
+	LoggingConfig.configure()
+	install(CallLogging) {
+		format { call ->
+			val status = call.response.status()
+			val httpMethod = call.request.httpMethod.value
+			"$httpMethod ${call.request.path()} -> $status"
+		}
+		mdc("method") { call ->
+			call.request.httpMethod.value
+		}
+		mdc("path") { call ->
+			call.request.path()
+		}
+		mdc("traceId") {
+			Span.current().spanContext.traceId
+		}
+		mdc("spanId") {
+			Span.current().spanContext.spanId
+		}
+	}
+
 	install(Koin) {
 		modules(applicationModule())
+	}
+	install(KtorServerTelemetry) {
+		setOpenTelemetry(OtelEnvironment.openTelemetry)
 	}
 
 	install(ContentNegotiation) {
--- aeon_app/shared/build.gradle.kts
+++ aeon_app/shared/build.gradle.kts
@@ -4,69 +4,69 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
-    kotlin("plugin.serialization") version "2.0.0"
+	alias(libs.plugins.kotlinMultiplatform)
+	alias(libs.plugins.androidLibrary)
+	kotlin("plugin.serialization") version "2.0.0"
 }
 
 kotlin {
-    androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
-    }
+	androidTarget {
+		@OptIn(ExperimentalKotlinGradlePluginApi::class)
+		compilerOptions {
+			jvmTarget.set(JvmTarget.JVM_11)
+		}
+	}
 
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
+	iosX64()
+	iosArm64()
+	iosSimulatorArm64()
 
-    jvm()
+	jvm()
 
-    @OptIn(ExperimentalWasmDsl::class)
-    wasmJs {
-        browser {
-            val rootDirPath = project.rootDir.path
-            val projectDirPath = project.projectDir.path
-            commonWebpackConfig {
-                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-                    static = (static ?: mutableListOf()).apply {
-                        // Serve sources to debug inside browser
-                        add(rootDirPath)
-                        add(projectDirPath)
-                    }
-                }
-            }
-        }
-    }
+	@OptIn(ExperimentalWasmDsl::class)
+	wasmJs {
+		browser {
+			val rootDirPath = project.rootDir.path
+			val projectDirPath = project.projectDir.path
+			commonWebpackConfig {
+				devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+					static = (static ?: mutableListOf()).apply {
+						// Serve sources to debug inside browser
+						add(rootDirPath)
+						add(projectDirPath)
+					}
+				}
+			}
+		}
+	}
 
-    sourceSets {
-        commonMain.dependencies {
-            api(libs.kotlinx.datetime)
-            api(libs.ktor.client.core)
-            api(libs.ktor.client.content)
-            api(libs.ktor.serialization.json)
-            api(libs.ktor.serialization.protobuf)
-            api(libs.ktor.client.logging)
-            api(libs.kotlinx.coroutines.core)
-        }
-        androidMain.dependencies {
-            implementation(libs.ktor.client.okhttp)
-        }
-        iosMain.dependencies {
-            implementation(libs.ktor.client.darwin)
-        }
-    }
+	sourceSets {
+		commonMain.dependencies {
+			api(libs.kotlinx.datetime)
+			api(libs.ktor.client.core)
+			api(libs.ktor.client.content)
+			api(libs.ktor.serialization.json)
+			api(libs.ktor.serialization.protobuf)
+			api(libs.ktor.client.logging)
+			api(libs.kotlinx.coroutines.core)
+		}
+		androidMain.dependencies {
+			implementation(libs.ktor.client.okhttp)
+		}
+		iosMain.dependencies {
+			implementation(libs.ktor.client.darwin)
+		}
+	}
 }
 
 android {
-    namespace = "dev.bitvictory.aeon.shared"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-    }
+	namespace = "dev.bitvictory.aeon.shared"
+	compileSdk = libs.versions.android.compileSdk.get().toInt()
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_11
+		targetCompatibility = JavaVersion.VERSION_11
+	}
+	defaultConfig {
+		minSdk = libs.versions.android.minSdk.get().toInt()
+	}
 }
