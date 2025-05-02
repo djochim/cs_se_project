47a89e720be80ecf6ebe9520fe105e987a9c2dbd
DamienJochim <dev@joch.im>
2025-05-02 07:41:02 +0200

    Add test to home view model

--- aeon_app/composeApp/src/commonMain/kotlin/dev/bitvictory/aeon/screens/home/HomeViewModel.kt
+++ aeon_app/composeApp/src/commonMain/kotlin/dev/bitvictory/aeon/screens/home/HomeViewModel.kt
@@ -1,17 +1,17 @@
 package dev.bitvictory.aeon.screens.home
 
 import dev.bitvictory.aeon.components.AbstractViewModel
-import dev.bitvictory.aeon.service.UserService
+import dev.bitvictory.aeon.service.IUserService
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-class HomeViewModel(userService: UserService): AbstractViewModel(userService) {
+class HomeViewModel(userService: IUserService): AbstractViewModel(userService) {
 	private val _uiState = MutableStateFlow(HomeUIState(isAuthenticated = isAuthenticated()))
 	val uiState: StateFlow<HomeUIState> = _uiState.asStateFlow()
 
 	fun changeQuery(newQuery: String) {
 		_uiState.value = _uiState.value.copy(query = newQuery)
 	}
-	
+
 }
