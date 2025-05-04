86b067b59fc3f0a516ca96a6dcea2b30562d85d4
Damien Jochim <dev@joch.im>
2025-05-04 21:09:17 +0200

    Add tests to build pipeline (#76)

--- aeon_app/composeApp/build.gradle.kts
+++ aeon_app/composeApp/build.gradle.kts
@@ -92,8 +92,8 @@ android {
 		applicationId = "dev.bitvictory.aeon"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 1
-		versionName = "1.0"
+		versionCode = 2
+		versionName = "1.1"
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
 	packaging {
