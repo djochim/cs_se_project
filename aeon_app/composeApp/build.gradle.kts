1dd103ba0e325b6604cbc756fbdc6e82802d0db7
DamienJochim <dev@joch.im>
2025-05-06 21:21:26 +0200

    Release new app

--- aeon_app/composeApp/build.gradle.kts
+++ aeon_app/composeApp/build.gradle.kts
@@ -92,8 +92,8 @@ android {
 		applicationId = "dev.bitvictory.aeon"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 2
-		versionName = "1.1"
+		versionCode = 3
+		versionName = "1.3"
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
 	packaging {
